9d09e47d756655af6ce2314dbeb452ec2dd07df8
uhidontkno <hi@rare1k.dev>
2024-07-03 19:59:54 -0400

    feat: fill in everything else with bogus

--- index.ts
+++ index.ts
@@ -1,7 +1,20 @@
 import { Builder, Browser, By, Key, until } from "selenium-webdriver";
-import { Select } from "selenium-webdriver/common/"
 import chrome from "selenium-webdriver/chrome";
-let data = {"creds":[Math.floor(Date.now()*(Math.random()*10)).toString(16),"Spam123456!"],"email":`spam${Date.now()}@xitroo.com`,"phone":[1,"123456789"]}
+
+const characters ='abcdefghijklmnopqrstuvwxyz';
+
+function generateString(length:number) {
+    let result = '';
+    const charactersLength = characters.length;
+    for ( let i = 0; i < length; i++ ) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+
+    return result;
+}
+
+
+let data = {"creds":[Math.floor(Date.now()*(Math.random()*10)).toString(16),"Spam123456!"],"email":`spam${generateString(12)}@xitroo.com`}
 
   let options = new chrome.Options()
   options.addArguments("--disable-dev-shm-usage","no-sandbox","disable-infobars","--disable-extensions")
@@ -19,4 +32,10 @@ let data = {"creds":[Math.floor(Date.now()*(Math.random()*10)).toString(16),"Spa
   // Whois/legal information
   await driver.findElement(By.id('newAccountEmail')).sendKeys(data.email);
   await driver.findElement(By.css('#newAccountCountry > option:nth-child(2)')).click();
-  
+  await driver.findElement(By.id('newAccountFirstName')).sendKeys(generateString(10));
+  await driver.findElement(By.id('newAccountAddress1')).sendKeys(generateString(32));
+  await driver.findElement(By.id('newAccountCity')).sendKeys(generateString(10));
+  await driver.findElement(By.id('newAccountZip')).sendKeys((Date.now()*Math.random()).toString().substring(0,5));
+  await driver.findElement(By.css('#newAccountState > option:nth-child(2)')).click();
+  await driver.findElement(By.id('newAccountPhone')).sendKeys((Date.now()*Math.random()).toString().substring(0,11));
+  await driver.findElement(By.css('#newAccountPhoneCode > option:nth-child(2)')).click();
