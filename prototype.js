0dd5f9bb80bf995077ff1e74e238c78e3e604a34
uhidontkno <hi@rare1k.dev>
2024-07-03 16:10:55 -0400

    feat: make sure you can actually pick the state

--- prototype.js
+++ prototype.js
@@ -8,8 +8,11 @@ document.querySelector("#newAccountEmail").value = data.email;
 document.querySelector("#newAccountFirstName").value = data.legal.firstName;
 document.querySelector("#newAccountAddress1").value = data.legal.address;
 document.querySelector("#newAccountCountry").selectedIndex = data.legal.country;
+processCountry(document.querySelector("#newAccountCountry"), 'newAccountState');
 document.querySelector("#newAccountCity").value = data.legal.city;
-document.querySelector("#newAccountState").selectedIndex = data.legal.state;
+setTimeout(()=>{
+    document.querySelector("#newAccountState").selectedIndex = data.legal.state;
+},500)
 document.querySelector("#newAccountZip").value = data.legal.zipCode;
 document.querySelector("#newAccountPhoneCode").selectedIndex = data.phone[0];
 document.querySelector("#newAccountPhone").value = data.phone[1];
