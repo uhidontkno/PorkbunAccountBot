2d29ab8c42ee682599ec38122d98cffed8f9f1ee
uhidontkno <hi@rare1k.dev>
2024-07-03 17:17:57 -0400

    feat: start working on filling in the info

--- index.ts
+++ index.ts
@@ -1,13 +1,8 @@
 import { Builder, Browser, By, Key, until } from "selenium-webdriver";
 import chrome from "selenium-webdriver/chrome";
-  
+let data = {"creds":[Math.floor(Date.now()*(Math.random()*10)).toString(16),"Spam123456!"],"email":`spam${Date.now()}@xitroo.com`,"legal":{"firstName":"test","address":"123 Testing Lane","country":1,"city":"Test","state":4,"zipCode":"123456"},"phone":[1,"123456789"]}
+
   let options = new chrome.Options()
-  options.addArguments("--disable-dev-shm-usage","no-sandbox")
+  options.addArguments("--disable-dev-shm-usage","no-sandbox","disable-infobars","--disable-extensions")
   let driver = await new Builder().forBrowser(Browser.CHROME).setChromeOptions(options).build()
-  try {
-    await driver.get('https://www.google.com/ncr')
-    await driver.findElement(By.name('q')).sendKeys('webdriver', Key.RETURN)
-    await driver.wait(until.titleIs('webdriver - Google Search'), 1000)
-  } finally {
-    await driver.quit()
-  }
+  await driver.get('https://porkbun.com/account/login')
