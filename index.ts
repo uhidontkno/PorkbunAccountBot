5a9a74b723e2aeb6e315b779537743748ede9eca
uhidontkno <hi@rare1k.dev>
2024-07-03 17:34:11 -0400

    feat: we're just gonna do bogus for whois info bc its gonna be easier

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import { Builder, Browser, By, Key, until } from "selenium-webdriver";
+import { Select } from "selenium-webdriver/common/"
 import chrome from "selenium-webdriver/chrome";
-let data = {"creds":[Math.floor(Date.now()*(Math.random()*10)).toString(16),"Spam123456!"],"email":`spam${Date.now()}@xitroo.com`,"legal":{"firstName":"test","address":"123 Testing Lane","country":1,"city":"Test","state":4,"zipCode":"123456"},"phone":[1,"123456789"]}
+let data = {"creds":[Math.floor(Date.now()*(Math.random()*10)).toString(16),"Spam123456!"],"email":`spam${Date.now()}@xitroo.com`,"phone":[1,"123456789"]}
 
   let options = new chrome.Options()
   options.addArguments("--disable-dev-shm-usage","no-sandbox","disable-infobars","--disable-extensions")
@@ -14,3 +15,8 @@ let data = {"creds":[Math.floor(Date.now()*(Math.random()*10)).toString(16),"Spa
   // Login details
   await driver.findElement(By.id('newAccountUsername')).sendKeys(data.creds[0]);
   await driver.findElement(By.id('newAccountPassword')).sendKeys(data.creds[1]);
+
+  // Whois/legal information
+  await driver.findElement(By.id('newAccountEmail')).sendKeys(data.email);
+  await driver.findElement(By.css('#newAccountCountry > option:nth-child(2)')).click();
+  
