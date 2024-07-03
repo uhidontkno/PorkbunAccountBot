b35105d786a580a741d0f6631dd281c654277de0
rare1k <hi@rare1k.dev>
2024-07-03 16:02:19 -0400

    feat: Create prototype.js

---
+++ prototype.js
@@ -0,0 +1,16 @@
+// Replace the data variable with your info. The legal/whois information are not validated so you can just put in bogus.
+let data = {"creds":["testingSpam123","testingSp@m1233"],"email":"spam@rare1k.dev","legal":{"firstName":"test","address":"123 Testing Lane","country":1,"city":"Test","state":4,"zipCode":"123456"},"phone":[1,"123456789"]}
+document.querySelector("#tosAgreement").checked = true;
+document.querySelector("#newAccountSubscribeNo").checked = true;
+document.querySelector("#newAccountUsername").value = data.creds[0];
+document.querySelector("#newAccountPassword").value = data.creds[1];
+document.querySelector("#newAccountEmail").value = data.email;
+document.querySelector("#newAccountFirstName").value = data.legal.firstName;
+document.querySelector("#newAccountAddress1").value = data.legal.address;
+document.querySelector("#newAccountCountry").selectedIndex = data.legal.country;
+document.querySelector("#newAccountCity").value = data.legal.city;
+document.querySelector("#newAccountState").selectedIndex = data.legal.state;
+document.querySelector("#newAccountZip").value = data.legal.zipCode;
+document.querySelector("#newAccountPhoneCode").selectedIndex = data.phone[0];
+document.querySelector("#newAccountPhone").value = data.phone[1];
+accountCreateCheck();
