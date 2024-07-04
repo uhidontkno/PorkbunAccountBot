49dd50809240c045278a318a92e9d41519d074a4
uhidontkno <hi@rare1k.dev>
2024-07-03 20:24:00 -0400

    feat: start working on email verification

--- index.ts
+++ index.ts
@@ -12,9 +12,19 @@ function generateString(length:number) {
 
     return result;
 }
+const timeout = (delay: number | undefined) => new Promise(resolve => setTimeout(resolve, delay));
+async function getLatestXitroo(address:string) {
+  while (true) {
+    await timeout(2000)
+  let mail = await (await fetch(`https://api.xitroo.com/v1/mails?locale=en&mailAddress=${address}&mailsPerPage=1&minTimestamp=0&maxTimestamp=${Date.now() / 1000}`)).json();
+  if (!mail["totalMails"]) {continue;}
+  let email = await (await fetch(`https://api.xitroo.com/v1/mail?locale=en&id=${mail[0]["_id"]}`)).json()
+  return email["bodyHtmlStrict"]
+  }
+}
 
 
-let data = {"creds":[Math.floor(Date.now()*(Math.random()*10)).toString(16),"Spam123456!"],"email":`spam${generateString(12)}@xitroo.com`}
+let data = {"creds":[generateString(16),"Spam123456!"],"email":`spam${generateString(12)}@xitroo.com`}
 
   let options = new chrome.Options()
   options.addArguments("--disable-dev-shm-usage","no-sandbox","disable-infobars","--disable-extensions")
@@ -39,3 +49,8 @@ let data = {"creds":[Math.floor(Date.now()*(Math.random()*10)).toString(16),"Spa
   await driver.findElement(By.css('#newAccountState > option:nth-child(2)')).click();
   await driver.findElement(By.id('newAccountPhone')).sendKeys((Date.now()*Math.random()).toString().substring(0,11));
   await driver.findElement(By.css('#newAccountPhoneCode > option:nth-child(2)')).click();
+  driver.executeScript(`accountCreateCheck();`)
+
+// Email verification
+await timeout(2000);
+console.log(await getLatestXitroo(data.email))
