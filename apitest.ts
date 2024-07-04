d89d000a0c271b26976fd5b035a986245b578f51
uhidontkno <hi@rare1k.dev>
2024-07-04 17:56:40 -0400

    feat: fix csrf & add a signup testing script

--- apitest.ts
+++ apitest.ts
@@ -1,3 +1,4 @@
+import { getLatestXitroo,getCSRFToken } from "./helper.ts"
 function generateString(length:number) {
     const characters ='abcdefghijklmnopqrstuvwxyz';
       let result = '';
@@ -25,6 +26,7 @@ let r = await fetch("https://porkbun.com/api/user/accountCreateCheck", {
       "accept": "application/json, text/javascript, */*; q=0.01",
       "content-type": "application/x-www-form-urlencoded; charset=UTF-8",
       "x-requested-with": "XMLHttpRequest",
+      "cookie": `csrf_pb=${csrf}`
     },
     "body": `username=${username}&email=${encodeURIComponent(email)}&country=US&isajax=true&csrf_pb=${csrf}`,
     "method": "POST"
@@ -42,6 +44,7 @@ async function verifyEmail(email:string,code:string,csrf:string): Promise<(strin
           "accept": "application/json, text/javascript, */*; q=0.01",
           "content-type": "application/x-www-form-urlencoded; charset=UTF-8",
           "x-requested-with": "XMLHttpRequest",
+          "cookie": `csrf_pb=${csrf}`
         },
         "body": `email=${encodeURIComponent(email)}&code=${code}&isajax=true&csrf_pb=${csrf}`,
         "method": "POST"
@@ -58,8 +61,9 @@ async function signup(username:string,password:string,email:string,csrf:string):
         "headers": {
           "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
           "content-type": "application/x-www-form-urlencoded",
+          "cookie": `csrf_pb=${csrf}`
         },
-        "body": `login=1&clientFingerprint=27870f8acf0b769042946002e205bafa&clientFingersha=c405078025381c5db9f8ba0d82ad27180a7600fc2b571ed956882d1cd8447305&redir=&usePasskey=0&passkeyData=&newAccountUsername=${username}&newAccountPassword=${encodeURIComponent(password)}&email=${encodeURIComponent(email)}&emailBackup=&firstName=adsfa&lastName=&companyName=&address1=${generateString(24)}&address2=&address3=&country=US&city=Melbourne&state=Florida&zip=329${Math.floor(Math.random()*99)}&phoneCode=1%3AUS&phone=%28321%29+${Math.floor(Math.random()*999)}-1${Math.floor(Math.random()*999)}&tosAgreement=YES&newAccountSubscribe=NO&csrf_pb=${csrf}`,
+        "body": `login=1&clientFingerprint=27870f8acf0b769042946002e205bafa&clientFingersha=c405078025381c5db9f8ba0d82ad27180a7600fc2b571ed956882d1cd8447305&redir=&usePasskey=0&passkeyData=&newAccountUsername=${username}&newAccountPassword=${encodeURIComponent(password)}&email=${encodeURIComponent(email)}&emailBackup=&firstName=${generateString(12)}&lastName=&companyName=&address1=${generateString(24)}&address2=&address3=&country=US&city=Melbourne&state=Florida&zip=329${Math.floor(Math.random()*99)}&phoneCode=1%3AUS&phone=%28321%29+${Math.floor(Math.random()*999)}-1${Math.floor(Math.random()*999)}&tosAgreement=YES&newAccountSubscribe=NO&csrf_pb=${csrf}`,
         "method": "POST"
       });
       let cookies = r.headers.getSetCookie();
@@ -69,3 +73,19 @@ async function signup(username:string,password:string,email:string,csrf:string):
       }
       return [newCsrf,r]
 }
+
+let csrf = await getCSRFToken();
+let email = `spam${generateString(10)}@xitroo.com`;
+let creds = [generateString(12),generatePassword(12)]
+let initial = await initalSignup(creds[0],email,csrf || "");
+csrf = String(initial[0]);
+// @ts-expect-error
+console.log(initial[1].status,await initial[1].text())
+let code = await getLatestXitroo(email);
+let emailVerif = await verifyEmail(email,code,csrf || "");
+csrf = String(emailVerif[0]);
+// @ts-expect-error
+console.log(emailVerif[1].status,await emailVerif[1].text())
+let created = await signup(creds[0],creds[1],email,csrf)
+// @ts-expect-error
+console.log(created[1].status)
