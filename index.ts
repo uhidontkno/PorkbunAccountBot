8f8102953fb06c300d8f871bafb4d35a2f6c5a63
uhidontkno <hi@rare1k.dev>
2024-07-03 22:35:31 -0400

    feat: try and humanify some info & bypass error 001

--- index.ts
+++ index.ts
@@ -23,12 +23,35 @@ async function getLatestXitroo(address:string) {
   }
 }
 
+let ua = ["Mozilla/5.0 (iPhone; CPU iPhone OS 16_5 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.5 Mobile/15E148 Safari/604.1",
+"Mozilla/5.0 (iPad; CPU OS 16_5 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.5 Mobile/15E148 Safari/604.1",
+"Mozilla/5.0 (iPhone; CPU iPhone OS 16_5 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/114.0.5735.99 Mobile/15E148 Safari/604.1",
+"Mozilla/5.0 (iPad; CPU OS 16_5 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/114.0.5735.124 Mobile/15E148 Safari/604.1",
+"Mozilla/5.0 (iPhone; CPU iPhone OS 16_5 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) FxiOS/114.1 Mobile/15E148 Safari/605.1.15",
+"Mozilla/5.0 (iPhone; CPU iPhone OS 16_5 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.0 YaBrowser/23.5.6.403.10 SA/3 Mobile/15E148 Safari/604.1",
+"Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Mobile Safari/537.36",
+"Mozilla/5.0 (Linux; Android 13; SAMSUNG SM-S918B) AppleWebKit/537.36 (KHTML, like Gecko) SamsungBrowser/21.0 Chrome/110.0.5481.154 Mobile Safari/537.36",
+"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36 OPR/99.0.0.0",
+"Opera/9.80 (Android; Opera Mini/7.5.33942/191.308; U; en) Presto/2.12.423 Version/12.16",
+"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/534.24 (KHTML, like Gecko) Chrome/100.0.4896.127 Safari/534.24 XiaoMi/MiuiBrowser/13.30.1-gn",
+"Mozilla/5.0 (Linux; Android 10; JNY-LX1; HMSCore 6.11.0.302) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.88 HuaweiBrowser/13.0.5.303 Mobile Safari/537.36",
+"Mozilla/5.0 (Linux; U; Android 11; en-us; itel P661W Build/RP1A.201005.001) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.85 Mobile Safari/537.36 PHX/12.9",
+"Mozilla/5.0 (Linux; Android 13; SM-S918B) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Mobile Safari/537.36 EdgA/113.0.1774.63",
+"Mozilla/5.0 (Linux; arm_64; Android 12; CPH2205) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 YaBrowser/23.3.3.86.00 SA/3 Mobile Safari/537.36",
+"Mozilla/5.0 (Linux; U; Android 4.4.2; en-US; HM NOTE 1W Build/KOT49H) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 UCBrowser/11.0.5.850 U3/0.8.0 Mobile Safari/534.30",
+"Mozilla/5.0 (Linux; Android 8.1.0; vivo 1820 Build/O11019; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/87.0.4280.141 Mobile Safari/537.36 VivoBrowser/10.4.2.0",
+"Mozilla/5.0 (Linux; U; Android 13; vi-vn; CPH2159 Build/TP1A.220905.001) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.88 Mobile Safari/537.36 HeyTapBrowser/45.9.5.1.1",
+"Mozilla/5.0 (Android 13; Mobile; rv:109.0) Gecko/114.0 Firefox/114.0",
+"Mozilla/5.0 (Linux; Android 5.1.1; KFSUWI) AppleWebKit/537.36 (KHTML, like Gecko) Silk/108.4.6 like Chrome/108.0.5359.220 Safari/537.36"
+]
 
-let data = {"creds":[generateString(16),"Spam123456!"],"email":`spam${generateString(12)}@xitroo.com`}
+let data = {"creds":[generateString(8),"A@BB"+generateString(15)],"email":`spam${generateString(12)}@xitroo.com`}
 
   let options = new chrome.Options()
-  options.addArguments("--disable-dev-shm-usage","no-sandbox","disable-infobars","--disable-extensions","--incognito","--remote-debugging-port=9222","--disable-dev-shm-using")
+  options.addArguments("--disable-dev-shm-usage","no-sandbox","disable-infobars","--disable-extensions","--remote-debugging-port=9222","--disable-dev-shm-using",`user-agent=${ua[Math.floor(Math.random()*ua.length)]}`)
   let driver = await new Builder().forBrowser(Browser.CHROME).setChromeOptions(options).build()
+  let driver2 = await new Builder().forBrowser(Browser.CHROME).setChromeOptions(options).build()
+  await driver2.get("https://ablankpage.pages.dev");
   await driver.get('https://porkbun.com/account/login');
   await timeout(1000);
   if (await driver.getTitle() == "Human Verification") {
@@ -46,12 +69,12 @@ let data = {"creds":[generateString(16),"Spam123456!"],"email":`spam${generateSt
   // Whois/legal information
   await driver.findElement(By.id('newAccountEmail')).sendKeys(data.email);
   await driver.findElement(By.css('#newAccountCountry > option:nth-child(2)')).click();
-  await driver.findElement(By.id('newAccountFirstName')).sendKeys(generateString(10));
+  await driver.findElement(By.id('newAccountFirstName')).sendKeys(generateString(12));
   await driver.findElement(By.id('newAccountAddress1')).sendKeys(generateString(32));
-  await driver.findElement(By.id('newAccountCity')).sendKeys(generateString(10));
-  await driver.findElement(By.id('newAccountZip')).sendKeys((Date.now()*Math.random()).toString().substring(0,5));
-  await driver.findElement(By.css('#newAccountState > option:nth-child(2)')).click();
-  await driver.findElement(By.id('newAccountPhone')).sendKeys("555-155-4761");
+  await driver.findElement(By.id('newAccountCity')).sendKeys("Roseland");
+  await driver.findElement(By.id('newAccountZip')).sendKeys("32957");
+  await driver.findElement(By.css('#newAccountState > option:nth-child(10)')).click();
+  await driver.findElement(By.id('newAccountPhone')).sendKeys("321-551-1234"); // phone number likely does not exist
   await driver.findElement(By.css('#newAccountPhoneCode > option:nth-child(2)')).click();
   driver.executeScript(`accountCreateCheck();`)
 
